cace9b9d85dafa5d06b0a70559cc2f0deac426f9
AarghCreate <67373626+AarghCreate@users.noreply.github.com>
2021-01-02 11:48:15 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,15 +1,33 @@
 var PassCode;
+var LOGO
+function preload()
+{
+    LOGO = loadImage( "LOGO.png" );
+}
 function setup()
 {
-    createCanvas( displayWidth , displayHeight );
+    createCanvas( 1350 , 740 );
     PassCode = prompt("PassCode");
 }
 function draw()
 {
-    if( PassCode === "ytrewq" )
+    if( PassCode === "wfuhefinsi" )
     {
-        background( "black" );
-    }else
+        background( LOGO );
+        push();
+        fill( "white" );
+        textSize( 20 );
+        text( "Your ID is 0001" , 630 , 50 );
+        pop();
+    }else if( PassCode === "lrkynfieng" )
+    {
+        background( LOGO );
+        push();
+        fill( "white" );
+        textSize( 20 );
+        text( " Your ID is 0002" , 630 , 50 );
+        pop();
+    }else 
     {
         close();
     }
